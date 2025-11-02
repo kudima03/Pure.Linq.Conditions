b55243ac7e4b1cd5b8d6a6bbaa7a865aaa28bab2
Dmitry Kurochkin <kudima03@gmail.com>
2025-11-02 17:33:20 +0300

    Improved EmptyConditionTests

--- src/Tests/Pure.Linq.Conditions.Tests/EmptyConditionTests.cs
+++ src/Tests/Pure.Linq.Conditions.Tests/EmptyConditionTests.cs
@@ -1,3 +1,5 @@
+using Pure.Linq.Conditions.Tests.Fakes;
+
 namespace Pure.Linq.Conditions.Tests;
 
 public sealed record EmptyConditionTests
@@ -12,5 +14,15 @@ public sealed record EmptyConditionTests
     public void ProduceCorrectResultOnNotEmptyCollection()
     {
         Assert.False(new EmptyCondition<int>(Enumerable.Range(0, 10)).BoolValue);
+    }
+
+    [Fact]
+    public void NotEvaluateWithoutAccess()
+    {
+        EnumerableWithEvaluationMarker collection = new EnumerableWithEvaluationMarker();
+
+        _ = new EmptyCondition<int>(collection);
+
+        Assert.False(collection.Evaluated);
     }
 }
